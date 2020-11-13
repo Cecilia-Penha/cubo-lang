8d45677e6405ab1b89fc4d378096e86ffdbc6e53
Cecilia-Penha <ceciliapenha3@gmail.com>
2020-11-13 20:51:53 -0300

    condicional

---
+++ fonte/Cond.java
@@ -0,0 +1,20 @@
+package fonte;
+
+public class Cond {
+    public void igualD(double a, double b){
+        if (a == b){
+            System.out.println("igual");
+        }
+        else{
+            System.out.println("diferente");
+        }
+    }
+    public void igualS(String a, String b){
+        if (a == b){
+            System.out.println("igual");
+        }
+        else{
+            System.out.println("diferente");
+        }
+    }
+}
