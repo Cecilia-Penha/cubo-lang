67adfcbf06ef2e215d75daf0903c5db7ff51bea3
Cecilia-Penha <ceciliapenha3@gmail.com>
2020-11-13 20:02:38 -0300

    método para imprimir

---
+++ fonte/Math.java
@@ -0,0 +1,6 @@
+package fonte;
+
+public class Math {
+    
+    
+}
---
+++ fonte/Print.java
@@ -0,0 +1,15 @@
+package fonte;
+
+public class Print {
+    public void prints(String msg){
+        System.out.println(msg);
+    }
+
+    public void printd(double msg){
+        System.out.println(msg);
+    }
+
+    public void printi(int msg){
+        System.out.println(msg);
+    }
+}
