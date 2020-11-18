21a68e2f1c17a5d01aefc81b19ee8b309256e5f6
Cecilia-Penha <ceciliapenha3@gmail.com>
2020-11-18 19:41:40 -0300

    adicionar file

--- fonte/Main.java
+++ fonte/Main.java
@@ -1,11 +1,24 @@
 package fonte;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.io.File;
 
 
 public class Main {
     public static void main(String[]args){
+        try{
+            File file = new File("teste.txt");
+            Scanner scan = new Scanner(file);
+            while(scan.hasNextLine()){
+                String line = scan.nextLine();
+                System.out.println(line);
+            }   
+            scan.close();
+        }catch(Exception e){
+            System.out.println("Erro");
+        }
         Print imprime = new Print();
+        Var[] variavel = new Var[20];
 
     }
 }
