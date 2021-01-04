a46cd6549db287a69a89687f0bddd4e066677f60
Cecilia-Penha <ceciliapenha3@gmail.com>
2021-01-04 07:16:06 -0300

    add jar

--- fonte/Cubo.java
+++ fonte/Cubo.java
@@ -1,14 +1,15 @@
 package fonte;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 import java.io.File;
 
 
-public class Main {
+public class Cubo {
     public static void main(String[]args){
         try{
             File file = new File("teste.txt");
             Scanner scan = new Scanner(file);
+            Pensa pensa = new Pensa();
+            
             while(scan.hasNextLine()){
                 String line = scan.nextLine();
                 System.out.println(line);
@@ -17,10 +18,8 @@ public class Main {
         }catch(Exception e){
             System.out.println("Erro");
         }
-        Print imprime = new Print();
-        Var[] variavel = new Var[20];
-        Cond testa = new Cond();
-        Math op = new Math();
+        
+        
 
        
 
--- fonte/Pensa.java
+++ fonte/Pensa.java
@@ -1,7 +1,32 @@
 package fonte;
 
 public class Pensa {
-    public void addVar(Var v){
+    public int i = 0;
+    public char op;
+    Print imprime = new Print();
+    Var[] variavel = new Var[20];
+    Cond testa = new Cond();
+    Math math = new Math();
+    String[] text;
+    
+    public void leia(){
+
+        while(i<=text.length){
+            if(text[i] == "break"){
+                break;
+            }
+            else if(text[i] == "start"){
+                this.start();
+            }
+            i++;
+        }
+    }
+    public void setOp(char conta){
+        this.op = conta;
+    }
+    public void start(){
         
     }
+
+
 }
